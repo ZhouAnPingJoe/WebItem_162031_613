7a46268a437df0cd66253c981fb0ba814599a34b
S16203125 <xiaohui>
2019-06-09 23:19:08 +0800

    第一次提交项目

---
+++ WebItem/src/main/java/webadv3/WebItem/zxh/system/HomeController.java
@@ -0,0 +1,14 @@
+package webadv3.WebItem.zxh.system;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+	
+
+}
+
